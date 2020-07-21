2e8278ef36369d22250cbcbf3e775d5f187370cf
Lesly Juarez <leslyj2@gmail.com>
2020-07-21 23:22:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
 learn spec/01_book_spec.rb 
 
+class Book
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author 
+    @author 
+  end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  def page_count
+    @page_count 
+  end
+  
+end
